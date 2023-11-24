e3dbbde81e59eb73bfb835748a216cd9deb0340f
Trevor Moyer <trevorm4@fb.com>
2023-11-24 12:46:57 -0700

    dont reply to responses to embed

--- internal/gpt/gpt.go
+++ internal/gpt/gpt.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 
@@ -57,6 +58,10 @@ func (g *Gpt) Handler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 	for _, mention := range m.Mentions {
 		if mention.ID == s.State.User.ID {
+			refMsg := m.ReferencedMessage
+			if refMsg != nil && refMsg.Author.ID == s.State.User.ID && strings.Contains(refMsg.Content, "vxtwitter") {
+				continue
+			}
 			response, err := g.send(m.Content)
 			if err != nil {
 				log.Println(err)
